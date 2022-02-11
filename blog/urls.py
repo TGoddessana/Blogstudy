b903d71567e7bc62d3b8f125415386a8aae7699a
TGoddessana <ingstor1092@gmail.com>
2022-02-11 22:45:25 +0900

    CBV로 리스트 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views # 같은 디렉토리의 views를 import
 
 urlpatterns = [
-    path('', views.index), # blog/ 면 views의 index() 함수를 수행
-    path('<int:pk>/', views.single_post_page),
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    # print(posts)
-    # <QuerySet [<Post: [1] This is first post.>]>
 
-    return render(
-        request,
-        'blog/post_list.html',
-        {
-            'posts': posts,
-        }
-    )
-
-def single_post_page(request, pk): # pk도 매개변수로 받음.
-    post = Post.objects.get(pk=pk) # pk가 매개변수로 받은 pk와 같은 레코드를 가져와라! 라는 의미
-    # pk는 primary key
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post, # 위에서 받아온 레코드
-        }
-    )
+class PostList(ListView):
+    model = Post # model을 정해 줌.
+    ordering = '-pk'
