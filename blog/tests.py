06dbc17d3e95f3309f175da175809121bcd016f1
TGoddessana <ingstor1092@gmail.com>
2022-03-19 22:23:06 +0900

    포스트 수정 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -10,7 +10,7 @@ class TestView(TestCase):
         self.user_user1 = User.objects.create_user(username='user1', password='somepassword')
         self.user_user2 = User.objects.create_user(username='user2', password='somepassword')
 
-        # 사용자에 스태프 권한 부여
+        # 사용자1에 스태프 권한 부여
         self.user_user1.is_staff = True
         self.user_user1.save()
 
@@ -242,3 +242,51 @@ class TestView(TestCase):
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, 'user1')
+
+    def test_update_post(self):
+
+        # 포스트 003 작성자 = user2
+
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
+
+        # 로그인하지 않은 경우에는 수정 페이지에 접근이 불가능해야 함
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.status_code, 200)
+
+        # 로그인은 했지만 작성자가 아닌 경우에는 수정 페이지에 접근했을 때에 403 코드(권한 없음) 이 나타나야 함
+        self.assertNotEqual(self.post_003.author, self.user_user1) # 포스트3의 작성자가 user2가 아닌지 확인
+        self.client.login(
+            username=self.user_user1.username,
+            password='somepassword'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+
+        # 작성자가 접근하는 경우, 수정이 가능해야 함
+        self.client.login(
+            username=self.post_003.author.username,
+            password='somepassword'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                'title' : '세 번째 포스트 수정하기',
+                'content' : '안녕하세요',
+                'category' : self.category_python.pk
+            },
+            follow=True
+        )
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트 수정하기', main_area.text)
+        self.assertIn('안녕하세요', main_area.text)
+        self.assertIn(self.category_python.name, main_area.text)
