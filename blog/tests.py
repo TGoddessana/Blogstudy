73307cf1309493a12dfb9684ff498fa01bea9a50
TGoddessana <ingstor1092@gmail.com>
2022-03-26 17:41:48 +0900

    포스트 작성 시에 텍스트로 태그 지정하기

--- blog/tests.py
+++ blog/tests.py
@@ -232,16 +232,28 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Create New Post', main_area.text)
 
+        # 태그 테스트
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+
         self.client.post(
             '/blog/create_post/',
             {
                 'title' : 'Post Form 만들기',
                 'content' : 'Post Form 페이지를 만듭시다.',
+                'tags_str' : 'new tag; 한글 태그, js'
             }
         )
+        self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, 'user1')
+
+        # 마지막 게시물의 태그가 3개가 맞는가?
+        self.assertEqual(last_post.tags.count(), 3)
+        self.assertTrue(Tag.objects.get(name='new tag'))
+        self.assertTrue(Tag.objects.get(name='한글 태그'))
+        self.assertEqual(Tag.objects.count(), 5)
 
     def test_update_post(self):
 
